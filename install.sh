dbe27867827b8b20ef6110ce0b305fc8f66bac5a
pengyongsheng <pys1024@sina.com>
2021-05-14 09:24:50 +0800

    Add support on windows.

--- install.sh
+++ install.sh
@@ -3,13 +3,19 @@
 
 set -e 
 
-if [ x"-a" = x"$1" ]; then
-  # install for all users
-  sudo cp OTF/ /usr/share/fonts/Source-code-pro -rf
-  sudo fc-cache
+info=$(uname -a)
+
+if [[ $info =~ "Linux" ]]; then
+    if [ x"-a" = x"$1" ]; then
+        # install for all users
+        sudo cp OTF/ /usr/share/fonts/Source-code-pro -rf
+        sudo fc-cache
+    else
+        # install for yourself
+        mkdir -p ~/.fonts
+        cp OTF/* ~/.fonts
+        fc-cache -f -v
+    fi
 else
-  # install for yourself
-  mkdir -p ~/.fonts
-  cp OTF/* ~/.fonts
-  fc-cache -f -v
+    cp TTF/ /c/Windows/Fonts -rf
 fi
